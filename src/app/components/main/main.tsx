e6d07a10e7a248384ed5bfc29319d6096596a8e9
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-08-27 18:08:24 -0300

    init

--- src/app/components/main/main.tsx
+++ src/app/components/main/main.tsx
@@ -6,7 +6,7 @@ export function Main() {
                 <p className="text-sm xl:text-lg text-justify pt-4 pb-4 font-normal">ISSO TEM QUE MUDAR (PROMESSA) Esta procurando uma terapia ou a sua parece não se encaixar? Você não está sozinho. Sua busca pelo equilíbrio emocional é única, e a terapia certa é essencial. Deixe-nos guiar você para uma jornada terapêutica feita para você.</p>
                 <button className="bg-azulescuro rounded-xl h-16 w-full cursor-pointer text-xl font-bold text-white">Clique e comece sua jornada!</button>
             </div>
-                <iframe className="flex-1 flex items-center justify-center w-full md:w-2/4 lg:h-2/4" src="https://www.youtube.com/embed/mX_BViO9OV4?si=nHhODwQTDqOkHHc2" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+                <iframe className="flex-1 flex items-center justify-center w-full md:w-3/4 lg:h-2/4" src="https://www.youtube.com/embed/mX_BViO9OV4?si=nHhODwQTDqOkHHc2" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
         </div>
     )
 }
